d41e8d1e5731c1b81909fd6e1cbc8a7ddeed3963
miladimos <milad.jafari6210@gmail.com>
2023-01-15 23:47:47 +0330

    Add Api, Webmaster projects

---
+++ src/Battler/Webmaster/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Webmaster.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
